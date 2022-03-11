3daf276880b928b8f2f72cb6092b75636cd44fe1
Goobrr <ggoobbrr@gmail.com>
2022-03-11 11:12:40 +0700

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+const rules = Vars.state.rules;
+
+Events.on(WorldLoadEvent.class, e => {
+    rules.dropZoneRadius = 150;
+    rules.waveSpacing = 4 * Time.toMinutes;
+
+    rules.buildCostMultiplier = 0.5;
+    rules.buildSpeedMultiplier = 2;
+    rules.deconstructRefundMultiplier = 1;
+
+    rules.blockHealthMultiplier = 2;
+    rules.blockDamageMultiplier = 2;
+
+    rules.unitBuildSpeedMultiplier = 1;
+
+    rules.winWave = Mathf.round(rules.winWave / 2);
+
+    rules.fire = false;
+    rules.damageExplosions = false;
+    rules.reactorExplosions = false;
+});
